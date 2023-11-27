c7346dd40de404722373b73d269dbf5d23a517f8
ygtozen <ozenyigit151@gmail.com>
2023-11-28 00:41:29 +0300

    first commit

---
+++ src/_01_Notes/Notes.java
@@ -0,0 +1,48 @@
+package _01_Notes;
+
+public class Notes {
+
+}
+
+//web serviceler farkli uygulamalarin birlikte calismasina olana saglar.
+//anahtar keliemiz -> interoperating kavrami (birlikte calisma)
+
+//farkli programlama dilide olsa ya da frrkli frameworkler de olsa fark etmez.
+//2 tane uygulama birbiri ile veri degisimi (data exchange) saglamak amacimiz.
+
+//ya da disariya public bir hizmet acilabilir.
+//hava durumu , currency , ip location web service ,
+//twitter , amazon vs
+
+//musteriye saglanan web hizmetleri ;
+
+//web service yardimiyla kendi bilgilerini parametre olarak vererek ,
+//transaction , order vs bilgilerini gorebilir...
+
+
+//Java da 2 tip web service vardir;
+//1- JAX-RS
+//2- JAX-WS
+//JAX-RS ve JAX-WS , JAVA EE 'in bir parcasidir/bolumudur.
+
+//####
+//JAX-RS
+//Java API for RESTful web services
+
+//lin uzerinden Spect'e erisim saglanabilir.
+//https://jcp.org/en/jsr/detail?id=339
+
+//JAX-RS 2.0, JSR-339, is a new JCP specification that provides a Java API for RESTful Web Services over the HTTP protocol.
+
+//JAX-RS implementations ;
+//1 - Jersey
+//2 - RESTEasy
+//3 - Restlet
+//4 - Apache CXF
+
+//REST
+
+//2000 yili , Roy Fielding , doktora tezinden ortaya cikmistir.
+//http://www.ics.uci.edu/~fielding/pubs/dissertation/top.htm
+
+//Representation State Transfer anlamina gelmektedir.
